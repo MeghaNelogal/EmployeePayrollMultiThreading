4f15e60738ca479cd7a217940ec8859e996656df
Your Name <you@example.com>
2022-09-08 20:57:18 +0530

    [Megha] Add: Added Multiple Employees Using Thread

--- EmployeePayrollMultiThreading/TestProject1/UnitTest1.cs
+++ EmployeePayrollMultiThreading/TestProject1/UnitTest1.cs
@@ -14,13 +14,26 @@ namespace TestProject1
             employeeDetails.Add(new EmployeeModel(employeeId: 3, employeeName: "shalu", phoneNumber: "9008088657", address: "hangl", departMent: "civil"));
             employeeDetails.Add(new EmployeeModel(employeeId: 4, employeeName: "chaitra", phoneNumber: "9008088657", address: "haveri", departMent: "physics"));
             employeeDetails.Add(new EmployeeModel(employeeId: 5, employeeName: "rahul", phoneNumber: "9008088657", address: "hubli", departMent: "EEE"));
+        
 
-            Operations emppayroll = new Operations();
+        Operations emppayroll = new Operations();
             DateTime StartDateTime = DateTime.Now;
             emppayroll.AddEmployee(employeeDetails);
             DateTime StopDateTimes = DateTime.Now;
 
             Console.WriteLine("Duration without threads: " + (StopDateTimes - StartDateTime));
+
+            DateTime StartDateTimeThread = DateTime.Now;
+            emppayroll.AddEmployeePayrollThread(employeeDetails);
+            DateTime StopDateTimeThread = DateTime.Now;
+
+            Console.WriteLine("Duration with threads: " + (StopDateTimeThread - StartDateTimeThread));
         }
     }
 }
+    
+
+
+
+
+    
